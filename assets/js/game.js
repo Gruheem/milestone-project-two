17069dc6ae17b38ea2da6670d413a282c6c9975f
Gruheem <grahamsilcock91@gmail.com>
2025-12-03 13:47:40 +0000

    Create API Call that Returns One Pokemon as an Object

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,18 @@
 async function getPokemonData(id) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const data = await response.json();
-    console.log(data);
-    return data;
+    // console.log(data);
+    let pokemonData = {
+        name: data.name,
+        image: data.sprites.other["official-artwork"].front_default,
+    };
+    console.log("fetched pokemon:", pokemonData);
+    return pokemonData;
 }
 
 getPokemonData(1)
+
+// async function createPokemonArray() {
+//     const pokemonArray = [];
+
+// }
