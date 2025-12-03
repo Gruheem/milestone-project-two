373c8e625bec4d5fb8287d4b28d6ba5e51467398
Gruheem <grahamsilcock91@gmail.com>
2025-12-03 14:01:27 +0000

    Create funtion() that Creates an Array of Pokemon

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,17 +2,25 @@ async function getPokemonData(id) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const data = await response.json();
     // console.log(data);
-    let pokemonData = {
+    const pokemonData = {
         name: data.name,
         image: data.sprites.other["official-artwork"].front_default,
     };
-    console.log("fetched pokemon:", pokemonData);
+    // console.log("fetched pokemon:", pokemonData);
     return pokemonData;
 }
 
-getPokemonData(1)
+// getPokemonData(1)
 
-// async function createPokemonArray() {
-//     const pokemonArray = [];
+async function createPokemonArray() {
+    const pokemonArray = [];
 
-// }
+    for (let i = 1; i <= 4; i++) { // true numnber to be taken from difficulty setting
+        const pokemon = await getPokemonData(Math.ceil(Math.random() * 151));
+        pokemonArray.push(pokemon);
+    }
+    console.log("pokemon Array:", pokemonArray);
+    return pokemonArray;
+}
+
+createPokemonArray();
